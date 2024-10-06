665540dce618c3d81d6d1750b42427966ff71a61
Sam <sriram.pant5@gmail.com>
2024-10-07 01:44:45 +0530

    Ch7 interfaces

--- ch6/coloredpoint.go
+++ ch6/coloredpoint.go
@@ -26,12 +26,47 @@ type ColoredPoint struct {
 }
 
 func main() {
+	p := Point{1, 2}
+	q := Point{4, 6}
+	distance := Point.Distance // method expression
+	fmt.Println(distance(p, q))
+	fmt.Printf("%T\n", distance)
+
+	scale := (*Point).ScaleBy
+	scale(&p, 2)
+	fmt.Println(p)
+	fmt.Printf("%T\n", scale)
+}
+
+func f2() {
+	p := Point{1, 2}
+	q := Point{4, 6}
+	distanceFromP := p.Distance // method value
+	// method value
+	fmt.Println(distanceFromP(q))
+	// "5"
+	var origin Point
+	// {0, 0}
+	fmt.Println(distanceFromP(origin)) // "2.23606797749979", ;5
+	scaleP := p.ScaleBy                // method value
+	scaleP(2)
+	fmt.Println(p)
+	scaleP(3)
+	fmt.Println(p)
+	scaleP(10)
+	fmt.Println(p)
+}
+
+func f1() {
 	red := color.RGBA{255, 0, 0, 255}
 	blue := color.RGBA{0, 0, 255, 255}
 	var p = ColoredPoint{Point{1, 1}, red}
 	var q = ColoredPoint{Point{5, 4}, blue}
 	fmt.Println(p.Distance(q.Point)) // "5"
 	p.ScaleBy(2)
+	fmt.Println(p)
 	q.ScaleBy(2)
+	fmt.Println(p)
 	fmt.Println(p.Distance(q.Point)) // "10"
+	fmt.Println(p)
 }
